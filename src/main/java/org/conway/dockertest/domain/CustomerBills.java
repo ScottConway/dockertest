f0c22df1e6dbf667cb790715d863e3a8a650ab0b
Scott Conway <scott.conway@gmail.com>
2020-10-22 17:02:49 -0500

    got last of domain classes implemented.

---
+++ src/main/java/org/conway/dockertest/domain/CustomerBills.java
@@ -0,0 +1,52 @@
+package org.conway.dockertest.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CustomerBills implements Serializable {
+    private long customerBillId;
+    private long customerAccountId;
+    private boolean isPaid;
+    private long amountDuePennies;
+    private Date dueDate;
+
+    public long getCustomerBillId() {
+        return customerBillId;
+    }
+
+    public void setCustomerBillId(long customerBillId) {
+        this.customerBillId = customerBillId;
+    }
+
+    public long getCustomerAccountId() {
+        return customerAccountId;
+    }
+
+    public void setCustomerAccountId(long customerAccountId) {
+        this.customerAccountId = customerAccountId;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public void setPaid(boolean paid) {
+        isPaid = paid;
+    }
+
+    public long getAmountDuePennies() {
+        return amountDuePennies;
+    }
+
+    public void setAmountDuePennies(long amountDuePennies) {
+        this.amountDuePennies = amountDuePennies;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+}
